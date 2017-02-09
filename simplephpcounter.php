1bbfa1eb26c7081c19274a3258a8ce0ec1a34f27
Charles Reace <cwreace@yahoo.com>
2017-02-08 20:34:30 -0500

    set/get total in file so scales better

--- simplephpcounter.php
+++ simplephpcounter.php
@@ -23,9 +23,14 @@ class Hits
   private function addHit($name)
   {
     $file = $this->hitDir.DIRECTORY_SEPARATOR.$name;
-    if(file_put_contents($file, '1', FILE_APPEND | LOCK_EX) == false) {
+    if(
+      file_put_contents(
+        $file,
+        ($hits = trim(file_get_contents($file)) + 1),
+        LOCK_EX
+      ) == false) {
       throw new Exception(__FILE__.': unable to write to '.$file);
     }
-    return filesize($file);
+    return $hits;
   }
 }
